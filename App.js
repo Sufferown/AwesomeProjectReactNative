281f4ca09aa573ff1dfba5cc2eda6d1fea86cc3e
Antonio Sepúlveda <51375386+Sufferown@users.noreply.github.com>
2023-08-14 00:47:53 -0400

    ¡Editando!
    Se realiza edición a código, agrego al silvestre. ¡Siempre en nuestros recuerdos!

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Esta es una versión de prueba de React Native</Text>
-      <Text>Saludos!</Text>
-      <StatusBar style="auto" />
+      <Image source={require('./Content/img/Silvestre.png')} />
+      <Text style={styles.title}>Prueba APP React Native</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>¡Gato!</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+  },
+  image: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#3498db',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
   },
 });
+
+export default App;
